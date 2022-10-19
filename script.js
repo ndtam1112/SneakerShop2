6c46e9d70ae81617e4116a6169df8d993ae47590
ndtam1112 <ndtam1010@gmail.com>
2022-10-20 03:31:25 +0700

    .

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header')
+    header.classList.toggle("sticky", window.scrollY > 0)
+})
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".intro-text",
+        start: "center center",
+        end: "bottom top",
+        markers: true,
+        scrub: true,
+        pin: true
+        }
+})
+
+.from(".text-1", { opacity: 0})
+.from(".text-2", { opacity: 0})
+.from(".img-1", { 
+    y : innerHeight * 1,
+    rotate: 360,
+    opacity: 0
+})
+
